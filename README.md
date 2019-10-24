55f7915bcadf04277b8877ee1bb79fa15c5f7582
mpizarro <maximilianopi@buenosaires.gob.ar>
2019-10-24 17:16:23 -0300

    Add postman collection and Stamp TSA2

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 ### Servicio para consumir Api Rest Blockchain Federal Argentina 
 
-*Consulta de Smart Contract Time Stamp Authority*
+*Smart Contract Time Stamp Authority*
 
-Microservicio Java generado con Jhipster para consumir Api Rest productivas del sello de tiempo de Blockchain Federal Argentina
+Microservicio Java generado con Jhipster para consumir las Api Rest del sello de tiempo de Blockchain Federal Argentina
+
+Instalación
+
+https://github.com/maximilianoPizarro/BlockchainService/blob/master/INSTALL.md
+
+Postman Collection
+
+https://github.com/maximilianoPizarro/BlockchainService/blob/master/blockchain service.postman_collection
+
+Endpoint ApiRest
+
+https://github.com/maximilianoPizarro/BlockchainService/blob/master/src/main/resources/config/application.yml
+
+Proxima mejora, abi y web3 para consulta directa con los Smart Contract
--- src/main/java/com/blockchain/app/web/rest/BlockchainResource.java
+++ src/main/java/com/blockchain/app/web/rest/BlockchainResource.java
@@ -1,12 +1,12 @@
 package com.blockchain.app.web.rest;
 
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.AbstractMap.SimpleEntry;
 
 import com.blockchain.app.domain.StampResponse;
 import com.blockchain.app.service.BlockchainService;
-
+import com.google.gson.Gson;
 
 import org.apache.http.ParseException;
 import org.slf4j.Logger;
@@ -64,5 +64,39 @@ public class BlockchainResource {
         return ResponseEntity.ok().body(blockchainService.altaBloque(blockchainService.hashFile(multipartfile)));
     }
 
+    /**
+	 * POST /tsa/stamp : Stamp Metadata tsa1
+	 *
+	 * @param String metadata
+	 * @return StampResponse
+	 * @throws UnsupportedEncodingException
+	 * @throws ParseException
+	 * @throws IOException
+	 */
     
+    @PostMapping("/tsa2/stamp")
+    public ResponseEntity<String> tsangStamp(@RequestParam("metadata") String metadata)
+            throws UnsupportedEncodingException, ParseException, IOException {
+            log.debug("Stamp Metadata TSA = "+metadata);
+            SimpleEntry<String,Integer > response = new SimpleEntry<String, Integer> ("status",blockchainService.altaBloqueTsaNG(blockchainService.hashDocumento(metadata)));
+        return ResponseEntity.ok().body(new Gson().toJson(response));
+    }
+
+    /**
+	 * POST /tsa/stamp/multipartfile : Stamp MultipartFile tsa1
+	 *
+	 * @param MultipartFile multipartfile
+	 * @return StampResponse
+	 * @throws UnsupportedEncodingException
+	 * @throws ParseException
+	 * @throws IOException
+	 */
+
+    @PostMapping("/tsa2/stamp/multipartfile")
+    public ResponseEntity<String> tsangStamp(MultipartFile multipartfile)
+            throws UnsupportedEncodingException, ParseException, IOException {
+            log.debug("Stamp File TSA = "+multipartfile.getOriginalFilename());
+            SimpleEntry<String,Integer > response = new SimpleEntry<String, Integer> ("status",blockchainService.altaBloqueTsaNG(blockchainService.hashFile(multipartfile)));
+        return ResponseEntity.ok().body(new Gson().toJson(response));
+    }
 }
